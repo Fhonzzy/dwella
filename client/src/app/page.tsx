075c390cd27eb0bf83b3cc8361860d9bc7e326e8
Kingsley <donchibs59@gmail.com>
2025-04-16 12:33:33 +0100

    Setup shadcn and deps

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <h1 className="text-5xl font-bold underline text-center">
-      Hello world!
-    </h1>
-  )
+    <>
+      <h1 className="text-5xl font-bold underline text-center">Hello world!</h1>
+      <Button variant={"default"}>Click Me</Button>
+    </>
+  );
 }
